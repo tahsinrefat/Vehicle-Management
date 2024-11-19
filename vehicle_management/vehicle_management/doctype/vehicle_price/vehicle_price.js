bd340da96b44c661c9dc959f759b93266db18785
tahsinrefat <tahsin92refat@gmail.com>
2024-11-19 17:57:19 +0600

    now if rows removed, prices update

--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.js
@@ -22,6 +22,9 @@ frappe.ui.form.on("Vehicle Items", {
         console.log('called');
         calculate_amount(frm, cdt, cdn);
         calculate_grand_total(frm);
+    },
+    other_items_remove: function(frm) {
+        calculate_grand_total(frm);
     }
 })
 
@@ -43,29 +46,18 @@ function calculate_amount(frm, cdt, cdn) {
     frappe.model.set_value(cdt, cdn, 'amount', total_amount);
 }
 
-// function calculate_grand_total(frm) {
-//     let grand_total_op = frm.doc.grand_total || 0;
-
-//     grand_total_op = grand_total_op+frm.doc.company_price+frm.doc.customer_price;
-
-//     frm.set_value('grand_total', grand_total_op);
-// }
-
-// function calculate_grand_total(frm, cdt, cdn) {
-//     let grand_total_op = frm.doc.grand_total || 0;
-//     let row = locals[cdt][cdn]; 
-
-//     grand_total_op = grand_total_op+ row.amount;
-
-//     frappe.model.set_value('grand_total', grand_total_op);
-// }
-
 function calculate_grand_total(frm) {
-    let grand_total_op = (frm.doc.company_price || 0) + (frm.doc.customer_price || 0);
+    let company_price_op = frm.doc.company_price || 0;
+    let customer_price_op = frm.doc.customer_price || 0;
+
+    let child_table_total = 0;
     if (frm.doc.other_items) {
         frm.doc.other_items.forEach(row => {
-            grand_total_op += row.amount || 0;
+            child_table_total += row.amount || 0;
         });
     }
-    frm.set_value('grand_total', grand_total_op);
+
+    let grand_total = company_price_op + customer_price_op + child_table_total;
+
+    frm.set_value('grand_total', grand_total);
 }
