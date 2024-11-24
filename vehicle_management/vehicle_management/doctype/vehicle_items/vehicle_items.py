bc6b4690b71da57f12953dc08eab32caab6fd1eb
tahsinrefat <tahsin92refat@gmail.com>
2024-11-24 13:33:16 +0600

    significant logic fix on child table.

--- vehicle_management/vehicle_management/doctype/vehicle_items/vehicle_items.py
+++ vehicle_management/vehicle_management/doctype/vehicle_items/vehicle_items.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, Tahsin Ahmed Refat and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class VehicleItems(Document):
-	pass
+    pass
--- vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
+++ vehicle_management/vehicle_management/doctype/vehicle_price/vehicle_price.py
@@ -7,18 +7,18 @@ from frappe.model.document import Document
 
 class VehiclePrice(Document):
 	# pass
-    def validate(self):
-        print('CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING')
-        print('CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING')
-        if not self.company_price or not self.customer_price:
-            frappe.throw("Company Price and Customer Price must be provided.")
-        self.sale_price = self.company_price + self.customer_price
-        
-        grand_total = self.sale_price
-        for item in self.other_items:
-            if not item.quantity or not item.rate:
-                frappe.throw(f"Quantity and Rate must be provided for {item.item}.")
-            item.amount = item.quantity * item.rate
-            grand_total += item.amount
+	def validate(self):
+		print('CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING')
+		print('CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING CALCULATING')
+		if not self.company_price or not self.customer_price:
+			frappe.throw("Company Price and Customer Price must be provided.")
+		self.sale_price = self.company_price + self.customer_price
+		
+		grand_total = self.sale_price
+		for item in self.other_items:
+			if not item.quantity or not item.rate:
+				frappe.throw(f"Quantity and Rate must be provided for {item.item}.")
+			item.amount = item.quantity * item.rate
+			grand_total += item.amount
 
-        self.grand_total = grand_total
+		self.grand_total = grand_total
